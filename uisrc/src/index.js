1d360e8452e7360145bf9ffeb24fabd60ac5339c
wangxiaoqing <xiaoqingwang@126.com>
2017-08-28 23:33:39 +0800

    use electron

--- uisrc/src/index.js
+++ uisrc/src/index.js
@@ -4,6 +4,8 @@ import Root from './env/root';
 import store,{sagaMiddleware} from './env/store';
 import rootSaga from './sagas';
 
+// const remote = require('electron').remote;
+// alert(`remote:${!!remote}`);
 
 sagaMiddleware.run(rootSaga);
 ReactDOM.render(
